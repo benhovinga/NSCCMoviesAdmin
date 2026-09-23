f80896392378b166aba4ec38784a44ed0ac3ec3d
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-09-23 09:02:04 -0300

    Create the Movie model

---
+++ MoviesAdmin/Models/MovieModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MoviesAdmin.Models
+{
+    public class MovieModel
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public string? Synopsis { get; set; }
+        public string? Genre { get; set; }
+        public string? Rating { get; set; }
+        public int RuntimeMinutes { get; set; }
+        public DateOnly ReleaseDate { get; set; }
+        public string? Director { get; set; }
+        public string? Producer { get; set; }
+        public string? Distributor  { get; set; }
+        public string? OriginalLanguage { get; set; }
+    }
+}
